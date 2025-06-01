6b0b4d0b9ca07d25f4f6371c3b034c90b7f3d385
Camrado <blackandwhite.b17@gmail.com>
2025-06-01 11:31:26 +0400

    TAsksView

--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -64,7 +64,7 @@ export default {
         },
         {
           icon: '💰',
-          title: 'Spend 10,000 AZN',
+          title: 'Spend 1,000 AZN',
           description: 'Hit your first spending milestone',
           completed: false
         },
